6c15ef68db6d8cc20193ba6728075a1d5af9f8e1
abatdorf <abatdorf@purdue.edu>
2020-02-27 15:33:02 -0500

    Grid works on click

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,11 +9,13 @@ public class Grid {
 	private float cellSize;
 
 	public Grid(int width, int height, float cellSize){
+
 		this.width = width;
 		this.height = height;
 		this.cellSize = cellSize;
 
 		gridArray = new int[width, height];
+
 		Debug.Log(width + " " + height);
 
 		for(int x = 0; x < gridArray.GetLength(0); x++){
@@ -32,4 +34,15 @@ public class Grid {
 		return new Vector3(x, y) * cellSize;
 	}
 
+	private void GetXY(Vector3 worldPosition, out int x, out int y){
+		x = Mathf.FloorToInt(worldPosition.x / cellSize);
+		y = Mathf.FloorToInt(worldPosition.y / cellSize);
+	}
+
+	public void SquareClick (Vector3 worldPosition){
+		int x, y;
+		GetXY(worldPosition, out x, out y);
+		gridArray[x, y]++;
+		Debug.Log(x + " " + y + " " + gridArray[x, y]);
+	}
 }
--- Assets/Scripts/TestObject.cs
+++ Assets/Scripts/TestObject.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class TestObject : MonoBehaviour
 {
-
-	private void Start(){
-		Grid grid = new Grid(4, 4, 10f);		
+	private Grid grid;
+	private void Start() {
+		grid = new Grid(4, 4, 10f);		
 	}
 
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)){
+			grid.SquareClick(GetMouseWorldPosition());
+		}
+	}
+
+	//recursive functions to obtain the mouse's current position
+	//MOVE ELSEWHERE
+
+	public static Vector3 GetMouseWorldPosition(){
+		Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+		vec.z = 0f;
+		return vec;
+	}
+	public static Vector3 GetMouseWorldPositionWithZ(){
+		return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+	}
+	public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera){
+		return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+	}
+	public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera){
+		Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+		return worldPosition;
+	}
 }
