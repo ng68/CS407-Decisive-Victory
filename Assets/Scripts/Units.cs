aef9ff485839ab0da3b3408e0c4fa90daf10eeb1
Edhballa7 <compton0@purdue.edu>
2020-02-27 21:10:47 -0500

    Implementing units

---
+++ Assets/Scripts/Units.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    public int health;
+    public int attack;
+    public int range;
+    //public float moveTime = 0.1f;
+
+    //private Animator animator;
+    private GameObject target;
+    private GameObject[] oppUnits;
+    private Units targetScript;
+    private bool takeTime;
+
+    public GameObject FindClosest()
+    {
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in oppUnits)
+        {
+            if (!go.activeSelf)
+                continue;
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animator = GetComponent<Animator>();
+        if (transform.gameObject.tag == "Enemy")
+        {
+            //ally = false;
+            oppUnits = GameObject.FindGameObjectsWithTag("Ally");
+        }
+        else if (transform.gameObject.tag == "Ally")
+        {
+            //ally = true;
+            oppUnits = GameObject.FindGameObjectsWithTag("Enemy");
+        }
+        else
+            Debug.Log("Need to assign ally or enemy");
+          
+        
+    }
+
+    void LoseHealth(int loss)
+    {
+        health -= loss;
+        if (health <= 0)
+        {
+            //transform.gameObject.tag = "Dead";
+            //Play death animation
+            transform.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        target = FindClosest();
+        if (target == null)
+        {
+            Debug.Log("Game over");
+            return;
+        }
+        targetScript = target.GetComponent<Units>();
+        if (!takeTime)
+        {
+            AttemptAction();
+            StartCoroutine(ReduceTime());
+        }
+    }
+
+    void Attack()
+    {
+        targetScript.LoseHealth(attack);
+    }
+
+    void Move()
+    {
+        Debug.Log("Too Far");
+    }
+
+    void AttemptAction ()
+    {
+        //If in range to attack
+        if (Mathf.Abs(target.transform.position.x - transform.position.x) <= range && Mathf.Abs(target.transform.position.y - transform.position.y) <= range)
+        {
+            Attack();
+        }else
+        {
+            Move();
+        }
+    }
+
+    IEnumerator ReduceTime()
+    {
+        takeTime = true;
+        yield return new WaitForSeconds(.1f);
+        takeTime = false;
+    }
+}
