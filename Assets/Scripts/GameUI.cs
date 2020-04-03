b32fa3a087beef364b852a90e32f4b4ea16078d9
Bsinghsh <bsinghsh@purdue.edu>
2020-04-03 14:06:06 -0700

    Revert "Merge branch 'master' of https://github.com/ng68/CS407-Decisive-Victory"
    This reverts commit d9147c4a2845e9004b567884481e21fec58ac2d8, reversing
    changes made to 5ce87b9c26df8bf7c19b4bcdbb27256ea9420f8c.

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -34,7 +34,6 @@ public class GameUI : MonoBehaviour
     [Header("Game Log Canvas")]
     [Space(10)]
     [SerializeField] private GameObject LogText;
-    [SerializeField] private GameObject LogField;
     #endregion
 
     #region Canvas
@@ -61,20 +60,13 @@ public class GameUI : MonoBehaviour
 
     //MAIN SECTION
 
-    public void AppendLog(string message) {
-            LogText.GetComponent<Text>().text += '\n' + message;
-            Canvas.ForceUpdateCanvases();
-            LogField.GetComponent<ScrollRect>().verticalNormalizedPosition = 0.0f;
-            Canvas.ForceUpdateCanvases();        
-    }
-
     public void  MouseClick(string buttonType)
     {
         if (buttonType == "Pause")
         {   
             Time.timeScale = 0.0f;
             pauseDialog.SetActive(true);
-            AppendLog("Game Paused!");
+            LogText.GetComponent<Text>().text += '\n' + "Game Paused!";
             pauseButton.SetActive(false);
             startButton.SetActive(false);
         }
@@ -86,7 +78,7 @@ public class GameUI : MonoBehaviour
                 startButton.SetActive(true);
             }
             pauseDialog.SetActive(false);
-            AppendLog("Game Resumed!");
+            LogText.GetComponent<Text>().text += '\n' + "Game Resumed!";
             pauseButton.SetActive(true);
         }
         if (buttonType == "Quit")
@@ -100,7 +92,7 @@ public class GameUI : MonoBehaviour
             Time.timeScale = 1.0f;
             startButton.SetActive(false);
             gameStarted = true;
-            AppendLog("Game Started");
+            LogText.GetComponent<Text>().text += '\n' + "Game Started!";
         }
 
         if (buttonType == "Retry")
@@ -112,13 +104,13 @@ public class GameUI : MonoBehaviour
         if (buttonType == "Settings")
         {
             // Enter Game Settings
-            AppendLog("Settings");
+            LogText.GetComponent<Text>().text += '\n' + "Game Settings!";
             pauseDialog.SetActive(false);
             settingsDialog.SetActive(true);
         }
         if(buttonType == "Back2")
         {
-            AppendLog("Back to Pause Menu!");
+            LogText.GetComponent<Text>().text += '\n' + "Back to Pause Menu!";
             settingsDialog.SetActive(false);
             pauseDialog.SetActive(true);
         }
